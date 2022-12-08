127279cf8bd25f6ed2e86ff23a9c2ccf61ac6cb5
Juan Guillermo Buitrago Calle <64021772+Juanbc2@users.noreply.github.com>
2022-12-08 11:18:30 -0500

    Delete firebase-config.js

--- src/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export default{
-  apiKey: "AIzaSyAV6-VQtzvQiwH6khvJPnl9FH76HLCWZlg",
-  authDomain: "dr-gato-veterinaria.firebaseapp.com",
-  projectId: "dr-gato-veterinaria",
-  storageBucket: "dr-gato-veterinaria.appspot.com",
-  messagingSenderId: "817363061387",
-  appId: "1:817363061387:web:2311983a59235aac9ca28e"
-}
-
